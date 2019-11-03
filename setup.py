0d96354869b6ad15afadd31737526cf096ca760b
Yurii Serhiichuk <savik.ne@gmail.com>
2019-11-03 23:00:13 +0200

    Cleanup setup.py and make sure some essential stuff is in there

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 CLASSIFIERS = [
     "Intended Audience :: Developers",
     "License :: OSI Approved :: MIT License",
@@ -14,6 +17,16 @@ CLASSIFIERS = [
 
 setup(
     name='adobe_analytics_2',
+    version='0.1',
+    license='GPL',
+    description='Adobe Analytics v2 API wrapper',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Julien Piccini',
+    author_email='piccini.julien@gmail.com',
+    url='https://github.com/pitchmuc/adobe_analytics_api_2.0',
+    download_url='https://github.com/pitchmuc/adobe_analytics_api_2.0/archive/master.zip',
+    keywords=['adobe', 'analytics', 'API', 'python'],
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -21,6 +34,8 @@ setup(
         'jwt>=0.6.1',
         'pathlib2>=2.3.5',
         'requests>=2.22.0',
+        'PyJWT>=1.7.1'
     ],
-    classifiers=CLASSIFIERS
+    classifiers=CLASSIFIERS,
+    python_requires='>=3.5'
 )
