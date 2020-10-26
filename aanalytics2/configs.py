6421079d46e17cbaf623dfa98a03c29d70d09e47
Yurii Serhiichuk <savik.ne@gmail.com>
2020-10-26 22:40:33 +0200

    Cleanup and improve code.

--- aanalytics2/configs.py
+++ aanalytics2/configs.py
@@ -1,11 +1,16 @@
+import json
+import os
+from pathlib import Path
+
 from typing import Optional
 
-from aanalytics2 import modules, find_path, config
+from .config import config_object, header
+from .paths import find_path
 
 
-def createConfigFile(verbose: object = False) -> None:
-    """Creates a `config_admin.json` file with the pre-defined configuration format to store
-    the access data in.
+def createConfigFile(verbose: bool = False, destination: str = 'config_analytics_template.json') -> None:
+    """Creates a `config_admin.json` file with the pre-defined configuration format
+    to store the access data in under the specified `destination`.
     """
     json_data = {
         'org_id': '<orgID>',
@@ -14,11 +19,12 @@ def createConfigFile(verbose: object = False) -> None:
         'secret': "<YourSecret>",
         'pathToKey': '<path/to/your/privatekey.key>',
     }
-    with open('config_analytics_template.json', 'w') as cf:
-        cf.write(modules.json.dumps(json_data, indent=4))
+    with open(destination, 'w') as cf:
+        cf.write(json.dumps(json_data, indent=4))
     if verbose:
         print(
-            f" file created at this location : {modules.os.getcwd()}{modules.os.sep}config_analytics.json")
+            f" file created at this location : {os.getcwd()}{os.sep}config_analytics.json"
+        )
 
 
 def importConfigFile(path: str) -> None:
@@ -33,19 +39,21 @@ def importConfigFile(path: str) -> None:
     "./config.json"
     "/my-folder/config.json"
     """
-    config_file_path: Optional[modules.Path] = find_path(path)
+    config_file_path: Optional[Path] = find_path(path)
     if config_file_path is None:
         raise FileNotFoundError(
-            f"Unable to find the configuration file under path `{path}`.")
+            f"Unable to find the configuration file under path `{path}`."
+        )
     with open(config_file_path, 'r') as file:
-        f = modules.json.load(file)
-        config.config_object["org_id"] = f['org_id']
-        if 'api_key' in f.keys():
-            config.config_object["client_id"] = f['api_key']
-            config.header["x-api-key"] = f['api_key']
-        elif 'client_id' in f.keys():
-            config.config_object["client_id"] = f['client_id']
-            config.header["x-api-key"] = f['client_id']
-        config.config_object["tech_id"] = f['tech_id']
-        config.config_object["secret"] = f['secret']
-        config.config_object["pathToKey"] = f['pathToKey']
+        provided_config = json.load(file)
+        provided_keys = provided_config.keys()
+        config_object["org_id"] = provided_config['org_id']
+        if 'api_key' in provided_keys:
+            config_object["client_id"] = provided_config['api_key']
+            header["x-api-key"] = provided_config['api_key']
+        elif 'client_id' in provided_keys:
+            config_object["client_id"] = provided_config['client_id']
+            header["x-api-key"] = provided_config['client_id']
+        config_object["tech_id"] = provided_config['tech_id']
+        config_object["secret"] = provided_config['secret']
+        config_object["pathToKey"] = provided_config['pathToKey']
