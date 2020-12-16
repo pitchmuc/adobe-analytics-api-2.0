7550cbc1b68e34ac0ea613feef5484482c133daf
Nicola G <24620670+omgMath@users.noreply.github.com>
2020-12-16 07:23:44 +0100

    Remove duplicate test

--- test/test_configuration.py
+++ test/test_configuration.py
@@ -22,15 +22,3 @@ def test_private_key_should_be_used_if_provided():
                           private_key=expected_private_key)
     config = aanalytics2.config_object
     assert aanalytics2.configs.get_private_key_from_config(config) == expected_private_key
-
-
-def test_private_key_should_be_used_if_provided():
-    expected_private_key = 'private_key'
-    aanalytics2.configure(org_id='org_id',
-                          tech_id='tech_id',
-                          secret='secret',
-                          path_to_key=None,
-                          client_id='client_id',
-                          private_key=expected_private_key)
-    config = aanalytics2.config_object
-    assert aanalytics2.configs.get_private_key_from_config(config) == expected_private_key
